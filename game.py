0053ecd234d9dc09f9f8c995effd5105fe663b13
Ryze <mpcm289@gmail.com>
2024-10-11 20:41:36 -0300

    game.py

--- game.py
+++ game.py
@@ -7,34 +7,30 @@ pygame.init()
 
 largura = 640
 altura = 480
+
 x = largura / 2
 y = altura / 2
 
-x_blue = randint(40, 600)
-y_blue = randint(50, 430)
+x_azul = randint(40, 600)
+y_azul = randint(50, 430)
+
+pontos = 0
+fonte = pygame.font.SysFont('arial', 40, True, True)
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("Ancestry")
+pygame.display.set_caption('Ancestry')
 relogio = pygame.time.Clock()
 
 while True:
     relogio.tick(30)
     tela.fill((0, 0, 0))
+    mensagem = f'Pontos: {pontos}'
+    texto_formatado = fonte.render(mensagem, True, (255, 255, 255))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            quit()
+            exit()
 
-        '''if event.type == KEYDOWN:
-            if event.key == K_a:
-                x = x - 20
-            if event.key == K_d:
-                x = x + 20
-            if event.key == K_w:
-                y = y - 20
-            if event.key == K_s:
-                y = y + 20'''
-    
     if pygame.key.get_pressed()[K_a]:
         x = x - 20
     if pygame.key.get_pressed()[K_d]:
@@ -43,12 +39,15 @@ while True:
         y = y - 20
     if pygame.key.get_pressed()[K_s]:
         y = y + 20
-        
-    ret_red = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
-    ret_blue = pygame.draw.rect(tela, (0, 0, 255), (x_blue, y_blue, 40, 50))
 
-    if ret_red.colliderect(ret_blue):
-        x_blue = randint(40, 600)
-        y_blue = randint(50, 430)
+    ret_vermelho = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
+    ret_azul = pygame.draw.rect(tela, (0, 0, 255), (x_azul, y_azul, 40, 50))
+
+    if ret_vermelho.colliderect(ret_azul):
+        x_azul = randint(40, 600)
+        y_azul = randint(50, 430)
+        pontos = pontos + 1
+
+    tela.blit(texto_formatado, (450, 40))
 
     pygame.display.update()
